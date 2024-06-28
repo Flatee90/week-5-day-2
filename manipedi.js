260b627229f933fe8afb20f357ee08eeb536bead
Folashade Lateef <flatee90@gmail.com>
2024-06-28 00:42:38 -0500

    update

--- manipedi.js
+++ manipedi.js
@@ -6,7 +6,7 @@ function mynailscheduler(){
     appointment = prompt("What day would you like to book?");
     style = prompt("Mani, pedi, both?");
     mynailsHeader.innerHTML = `someone booked a nail spa for you!`
-    mynailsParagraph.innerHTML = `Hi ${name}, you have a secret admire whom wanted to reveal themselves to you with a gift of ${gifts}. ${signed} wanted you to know that they deeply care about you and on this day while making a declaration of their affection for you. Happy Valentine's Day from ${signed} and My Cupid!`;
+    mynailsParagraph.innerHTML = `Hi ${name}, someone wants to treat you to a spa day for ${appointment} for ${style} we can't wait to see you!`;
     
   }
-    myanils.onclick = mynailscheduler
+    mynails.onclick = mynailscheduler
