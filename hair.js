665a495f141cf87c5f407add231639b5e871b695
Folashade Lateef <flatee90@gmail.com>
2024-07-13 21:13:41 -0500

    update

--- hair.js
+++ hair.js
@@ -3,8 +3,8 @@ function mommyhair(){
     let guest;
     name = prompt("What is your name?");
     guest = prompt("A mommy you want to treat?");
-    newyearsHeader.innerHTML = `You're Invited....`;
-    newyearsParagraph.innerHTML = `Hi ${guest}! ${name} would like to invite you to treat you to a day of pampering!  `;
+    mommyHeader.innerHTML = `You're Invited....`;
+  mommyhairParagraph.innerHTML = `Hi ${guest}! ${name} would like to invite you to treat you to a day of pampering!  `;
     
   }
-    newyearsButton.onclick = mommyhair
+    mommyhair.onclick = mommyhair
